9e20138f7b818728ec75d51060180c1ba8bd68c7
Martin Kapun <capoony@gmail.com>
2021-11-11 15:44:07 +0100

    update

--- shell/workflow.sh
+++ shell/workflow.sh
@@ -1,6 +1,6 @@
 ### investigate with nanoplot
 
-mkdir ~/github/MinION_barcoding/results
+mkdir /media/inter/mkapun/projects/MinION_barcoding/results
 
 echo '''
 #!/bin/sh
@@ -9,7 +9,7 @@ echo '''
 #PBS -N Nanoplot
 
 ## Redirect output stream to this file.
-#PBS -o ~/github/MinION_barcoding/NanoPlot_log.txt
+#PBS -o /media/inter/mkapun/projects/MinION_barcoding/NanoPlot_log.txt
 
 ## Stream Standard Output AND Standard Error to outputfile (see above)
 #PBS -j oe
@@ -19,7 +19,7 @@ echo '''
 
 ######## load dependencies #######
 
-# first NanoPlot  into your environemt
+# first load NanoPlot into your environemt
 source /opt/anaconda3/etc/profile.d/conda.sh
 conda activate nanoplot_1.32.1
 
@@ -29,17 +29,38 @@ conda activate nanoplot_1.32.1
 
 NanoPlot \
   --fastq Supplementary_File1_reads.fastq \
-  -o ~/github/MinION_barcoding/results/nanoplot \
+  -o /media/inter/mkapun/projects/MinION_barcoding/results/nanoplot \
   -p subset
 
-''' > ~/github/MinION_barcoding/shell/NanoPlot_qsub.sh
+''' > /media/inter/mkapun/projects/MinION_barcoding/shell/NanoPlot_qsub.sh
 
-qsub ~/github/MinION_barcoding/shell/NanoPlot_qsub.sh
+qsub /media/inter/mkapun/projects/MinION_barcoding/shell/NanoPlot_qsub.sh
 
 
 ## Demultiplex samples
+echo '''
+#!/bin/sh
 
-python minibar.py \
+## name of Job
+#PBS -N Nanoplot
+
+## Redirect output stream to this file.
+#PBS -o /media/inter/mkapun/projects/MinION_barcoding/NanoPlot_log.txt
+
+## Stream Standard Output AND Standard Error to outputfile (see above)
+#PBS -j oe
+
+## Select a maximum of 20 cores and 200gb of RAM
+#PBS -l select=1:ncpus=20:mem=200gb
+
+######## load dependencies #######
+
+source /opt/venv/minibar/bin/activate
+module load Tools/minibar
+
+######## run analyses #######
+
+minibar.py \
   Supplementary_File3_primer.txt \
   Supplementary_File1_reads.fastq \
   -T \
@@ -47,7 +68,11 @@ python minibar.py \
   -e 3 \
   -E 11 \
 
-Expected results:
+  ''' > /media/inter/mkapun/projects/MinION_barcoding/shell/minibar_qsub.sh
+
+qsub /media/inter/mkapun/projects/MinION_barcoding/shell/minibar_qsub.sh
+
+## Expected results:
 
 ### load NGSspeciesID
 
@@ -58,7 +83,7 @@ echo '''
 #PBS -N NGSspeciesID
 
 ## Redirect output stream to this file.
-#PBS -o ~/github/MinION_barcoding/NGSspeciesID_log.txt
+#PBS -o /media/inter/mkapun/projects/MinION_barcoding/NGSspeciesID_log.txt
 
 ## Stream Standard Output AND Standard Error to outputfile (see above)
 #PBS -j oe
@@ -80,16 +105,33 @@ NGSpeciesID  \
   --s 100 \
   --t 20 \
   --medaka \
-  --primer_file ~/github/MinION_barcoding/data/test/Supplementary_File3_primer.txt \
-  --fastq ~/github/MinION_barcoding/data/subset.fastq \
-  --outfolder ~/github/MinION_barcoding/results/subset_consensus_medaka
+  --primer_file /media/inter/mkapun/projects/MinION_barcoding/data/test/Supplementary_File3_primer.txt \
+  --fastq /media/inter/mkapun/projects/MinION_barcoding/data/subset.fastq \
+  --outfolder /media/inter/mkapun/projects/MinION_barcoding/results/subset_consensus_medaka
 
-''' > ~/github/MinION_barcoding/shell/NGSspeciesID_qsub.sh
+''' > /media/inter/mkapun/projects/MinION_barcoding/shell/NGSspeciesID_qsub.sh
 
-qsub ~/github/MinION_barcoding/shell/NGSspeciesID_qsub.sh
+qsub /media/inter/mkapun/projects/MinION_barcoding/shell/NGSspeciesID_qsub.sh
 
 echo '''
 
+#!/bin/sh
+
+## name of Job
+#PBS -N NGSspeciesID
+
+## Redirect output stream to this file.
+#PBS -o /media/inter/mkapun/projects/MinION_barcoding/NGSspeciesID_log.txt
+
+## Stream Standard Output AND Standard Error to outputfile (see above)
+#PBS -j oe
+
+## Select a maximum of 20 cores and 200gb of RAM
+#PBS -l select=1:ncpus=20:mem=200gb
+
+######## load dependencies #######
+
+conda activate NGSpeciesID
 
 ######## run analyses - single sample and Racon #######
 
@@ -101,16 +143,33 @@ NGSpeciesID  \
   --s 100 \
   --t 20 \
   --racon \
-  --primer_file ~/github/MinION_barcoding/data/test/Supplementary_File3_primer.txt \
-  --fastq ~/github/MinION_barcoding/data/subset.fastq \
-  --outfolder ~/github/MinION_barcoding/results/subset_consensus_racon
+  --primer_file /media/inter/mkapun/projects/MinION_barcoding/data/test/Supplementary_File3_primer.txt \
+  --fastq /media/inter/mkapun/projects/MinION_barcoding/data/subset.fastq \
+  --outfolder /media/inter/mkapun/projects/MinION_barcoding/results/subset_consensus_racon
 
-''' > ~/github/MinION_barcoding/shell/NGSspeciesID_qsub.sh
+''' > /media/inter/mkapun/projects/MinION_barcoding/shell/NGSspeciesID_qsub.sh
 
-qsub ~/github/MinION_barcoding/shell/NGSspeciesID_qsub.sh
+qsub /media/inter/mkapun/projects/MinION_barcoding/shell/NGSspeciesID_qsub.sh
 
 echo '''
 
+#!/bin/sh
+
+## name of Job
+#PBS -N NGSspeciesID
+
+## Redirect output stream to this file.
+#PBS -o /media/inter/mkapun/projects/MinION_barcoding/NGSspeciesID_log.txt
+
+## Stream Standard Output AND Standard Error to outputfile (see above)
+#PBS -j oe
+
+## Select a maximum of 20 cores and 200gb of RAM
+#PBS -l select=1:ncpus=20:mem=200gb
+
+######## load dependencies #######
+
+conda activate NGSpeciesID
 
 ######## run analyses - mixed sample and Medaka #######
 
@@ -122,13 +181,13 @@ NGSpeciesID  \
   --s 100 \
   --t 20 \
   --medaka \
-  --primer_file ~/github/MinION_barcoding/data/test/Supplementary_File3_primer.txt \
-  --fastq ~/github/MinION_barcoding/data/subset.fastq \
-  --outfolder ~/github/MinION_barcoding/results/subset_consensus_mixed
+  --primer_file /media/inter/mkapun/projects/MinION_barcoding/data/test/Supplementary_File3_primer.txt \
+  --fastq /media/inter/mkapun/projects/MinION_barcoding/data/subset.fastq \
+  --outfolder /media/inter/mkapun/projects/MinION_barcoding/results/subset_consensus_mixed
 
-''' > ~/github/MinION_barcoding/shell/NGSspeciesID_qsub.sh
+''' > /media/inter/mkapun/projects/MinION_barcoding/shell/NGSspeciesID_qsub.sh
 
-qsub ~/github/MinION_barcoding/shell/NGSspeciesID_qsub.sh
+qsub /media/inter/mkapun/projects/MinION_barcoding/shell/NGSspeciesID_qsub.sh
 
 echo '''
 #!/bin/sh
@@ -137,7 +196,7 @@ echo '''
 #PBS -N BlastN
 
 ## Redirect output stream to this file.
-#PBS -o ~/github/MinION_barcoding/BlastN_log.txt
+#PBS -o /media/inter/mkapun/projects/MinION_barcoding/BlastN_log.txt
 
 ## Stream Standard Output AND Standard Error to outputfile (see above)
 #PBS -j oe
@@ -151,36 +210,82 @@ module load Alignment/ncbi-BLAST-2.12.0
 
 ######## run analyses #######
 
-mkdir ~/github/MinION_barcoding/results/blast
+mkdir /media/inter/mkapun/projects/MinION_barcoding/results/blast
 
 blastn \
   -num_threads 20 \
   -evalue 1e-100 \
   -outfmt "6 qseqid sseqid sscinames slen qlen pident length mismatch gapopen qstart qend sstart send evalue bitscore" \
-  -db /media/inter/scratch_backup/NCBI_nt_DB_210714/nt \
-  -query ~/github/MinION_barcoding/results/subset_consensus/consensus_reference_4.fasta \
-  > ~/github/MinION_barcoding/results/blast/blastn.txt
+  -db /media/scratch/NCBI_nt_DB_210714/nt \
+  -query /media/inter/mkapun/projects/MinION_barcoding/results/subset_consensus/consensus_reference_4.fasta \
+  > /media/inter/mkapun/projects/MinION_barcoding/results/blast/blastn.txt
 
-''' > ~/github/MinION_barcoding/shell/BlastN_qsub.sh
+''' > /media/inter/mkapun/projects/MinION_barcoding/shell/BlastN_qsub.sh
 
-qsub ~/github/MinION_barcoding/shellBlastN_qsub.sh
+qsub /media/inter/mkapun/projects/MinION_barcoding/shell/BlastN_qsub.sh
 
 ######## amplicon_sorter #######
+
+
+echo '''
+#!/bin/sh
+
+## name of Job
+#PBS -N ampliconsorter
+
+## Redirect output stream to this file.
+#PBS -o /media/inter/mkapun/projects/MinION_barcoding/ampliconsorter_log.txt
+
+## Stream Standard Output AND Standard Error to outputfile (see above)
+#PBS -j oe
+
+## Select a maximum of 20 cores and 200gb of RAM
+#PBS -l select=1:ncpus=20:mem=200gb
+
+######## load dependencies #######
+
+module load Tools/amplicon_sorter
+
 ######## run analyses - single sample  #######
 
 python3.6 amplicon_sorter.py \
-  -i ~/github/MinION_barcoding/data/subset.fastq \
-  -o ~/github/MinION_barcoding/results/subset_consensus_ampS \
+  -i /media/inter/mkapun/projects/MinION_barcoding/data/subset.fastq \
+  -o /media/inter/mkapun/projects/MinION_barcoding/results/subset_consensus_ampS \
   -np 4 \
   -min 600 \
   -max 1000 \
-  -maxr 1000 
+  -maxr 1000
+
+''' > /media/inter/mkapun/projects/MinION_barcoding/shell/ampliconsorter_qsub.sh
+
+qsub /media/inter/mkapun/projects/MinION_barcoding/shell/ampliconsorter_qsub.sh
+
+
+
+echo '''
+#!/bin/sh
+
+## name of Job
+#PBS -N ampliconsorter
+
+## Redirect output stream to this file.
+#PBS -o /media/inter/mkapun/projects/MinION_barcoding/ampliconsorter_log.txt
+
+## Stream Standard Output AND Standard Error to outputfile (see above)
+#PBS -j oe
+
+## Select a maximum of 20 cores and 200gb of RAM
+#PBS -l select=1:ncpus=20:mem=200gb
+
+######## load dependencies #######
+
+module load Tools/amplicon_sorter
 
 ######## run analyses - mixed sample  #######
 
 python3.6 amplicon_sorter.py \
-  -i ~/github/MinION_barcoding/data/subset.fastq \
-  -o ~/github/MinION_barcoding/results/subset_consensus_ampS \
+  -i /media/inter/mkapun/projects/MinION_barcoding/data/subset.fastq \
+  -o /media/inter/mkapun/projects/MinION_barcoding/results/subset_consensus_ampS \
   -np 4 \
   -min 600 \
   -max 1000 \
@@ -194,10 +299,10 @@ blastn \
   -num_threads 20 \
   -evalue 1e-100 \
   -outfmt "6 qseqid sseqid sscinames slen qlen pident length mismatch gapopen qstart qend sstart send evalue bitscore" \
-  -db /media/inter/scratch_backup/NCBI_nt_DB_210714/nt \
-  -query ~/github/MinION_barcoding/results/subset_consensus_ampS \
-  > ~/github/MinION_barcoding/results/blast/blastn.txt
+  -db /media/scratch/NCBI_nt_DB_210714/nt \
+  -query /media/inter/mkapun/projects/MinION_barcoding/results/subset_consensus_ampS \
+  > /media/inter/mkapun/projects/MinION_barcoding/results/blast/blastn.txt
 
-''' > ~/github/MinION_barcoding/shell/BlastN_qsub.sh
+''' > /media/inter/mkapun/projects/MinION_barcoding/shell/BlastN_qsub.sh
 
-qsub ~/github/MinION_barcoding/shellBlastN_qsub.sh
+qsub /media/inter/mkapun/projects/MinION_barcoding/shellBlastN_qsub.sh
